2a94662fd48bf5bc37bc259b54ebe2da0924ee41
Erik4052 <erik1999@live.com.mx>
2022-01-31 21:00:48 -0600

    fin sección 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,47 +1,22 @@
-import { NgModule, Component } from '@angular/core';
-import { Router, RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { PagesRoutingModule } from './pages/pages.routing';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
 
 const routes:Routes = [
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AuthRouting
+  //path: '/medicos' MedicosRouting
+  //path: '/compras' ComprasRouting
   {
-    path:'', component:PagesComponent,
-    children: [
-      {
-        path:'',
-        redirectTo:'/dashboard', pathMatch:'full'
-      },
-      {
-        path:'dashboard',
-        component:DashboardComponent
-      }, 
-      {
-        path:'progress',
-        component:ProgressComponent
-      },
-      {
-        path:'grafica1',
-        component:Grafica1Component
-      },
-    ]
+    path:'',
+    redirectTo:'/dashboard', pathMatch:'full'
   },
-  
-
-  {
-    path:'register',
-    component:RegisterComponent
-  },
-  {
-    path:'login',
-    component:LoginComponent
-  },
-  
   {
     path:'**',
     component:NopagefoundComponent
@@ -50,9 +25,10 @@ const routes:Routes = [
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[
     RouterModule
